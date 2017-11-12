37a9d473caa27c63c281c98bab6f8cefa2f90e0a
Pritam Sangani <pritam.naran@yahoo.co.uk>
2017-11-12 00:21:27 +0000

    built and trained ann. small changes to gamelogic file

--- battle_ann.py
+++ battle_ann.py
@@ -1,40 +1,63 @@
-#ANN
+#PART 1 - DATA PREPROCESSING
 
-#DATA PREPROCESSING
-import pandas as pd
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
- 
-#import dataset
-dataset = pd.read_csv("")
-X = dataset.iloc[:].values
-y = dataset.iloc[:].values
+import pandas as pd
 
-#encode data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# Importing the dataset
+dataset = pd.read_csv('BattleData.csv')
+X = dataset.iloc[:, :5].values
+y = dataset.iloc[:, 5].values
 
-#split dataset
+# Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
 
-#Scale data
+"""Feature Scaling - minimises risk of 1 independent variable dominating in the ANN 
+and ease heavy computations on your machine"""
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-#BUILD ANN
+#PART 2 - MAKING THE ARTIFICIAL NEURAL NETWORK
+
+#Importing the Keras library and packages
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
+#Initialising the ANN
 classifier = Sequential()
 
-classifier.add(Dense(activation='relu', input_dim=, units=, kernal_initializer='uniform'))
-classifier.add(Dense(activation='relu', units=, kernal_initializer='uniform'))
-classifier.add(Dense(activation='softmax', units=5, kernal_initializer='uniform'))
+#Adding the input layer and the first hidden layer
+classifier.add(Dense(activation = 'relu', input_dim = 5, units = 3, kernel_initializer = 'uniform'))
 
-classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-classifier.fit(X_train, y_train, batch_size=10, epochs=500)
+#Adding a second hidden layer
+classifier.add(Dense(activation = 'relu', units = 3, kernel_initializer = 'uniform'))
 
-#MAKE PREDICTIONS
+#Adding the output layer
+"""Use softmax activation function for output layer if more than 1 category - otherwise sigmoid"""
+classifier.add(Dense(activation = 'sigmoid', units = 1, kernel_initializer = 'uniform'))
+
+#Compiling the ANN
+"""Loss function if more than 1 output node is called 'categorical_crossentropy'
+otherwise if binary(either 1 or 0) use 'binary_crossentropy'"""
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+#Fitting the ANN to training set
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+
+#PART 3 - MAKING THE PREDICTIONS AND EVALUATING THE MODEL
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+#Categorise y_pred as being True or False (i.e. 1 or 0) for the confusion matrix
+y_pred = (y_pred > 0.5)
+
+
+new_prediction = classifier.predict(sc.transform(np.array([[0,1,1,0,0]])))
+new_prediction = (new_prediction > 0.5)
+
+
