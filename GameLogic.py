6f2dd4e12d4156544b818a0ceb66454095727859
YusofBandar <ybandar1902@gmail.com>
2017-11-12 11:57:27 +0000

    final

--- GameLogic.py
+++ GameLogic.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question
 import test
+import random
 app = Flask(__name__)
 ask = Ask(app, '/AlexaOfRings')
 
 playing = False
 attacker = ""
 player = -1
-
-
-
+attacked = False
 
 class monster:
 	name = ""
@@ -23,6 +22,7 @@ class Character:
 	type = ""
 	health = 100
 	stance = "NA"
+	luck = random.randint(1,100);
 	def __init__(self):
 	   health = 100
 
@@ -32,33 +32,30 @@ CharacterList = []
 temp = Character()
 temp.name = "Dave"
 temp.type = "Hobbit"
-temp.stance = "attack"
+temp.stance = "NA"
 CharacterList.append(temp)
 
-temp1 = Character()
+"""temp1 = Character()
 temp1.name = "Amy"
 temp1.type = "Wizard"
 temp1.stance = "attack"
-CharacterList.append(temp1)
+CharacterList.append(temp1)"""
 
-
-
-
-attacked = True
 @ask.launch
 def launched():
-	return question('Welcome to Alexa of Rings, would you like to a start')
+	return question('Welcome to Alexa of Rings, would you like to ... start')
 
 @ask.intent('StartGame')
 def startGame():
-	return question('Before we begin you need to create a character')
+	if(len(CharacterList) < 1):
+		return question('Before we begin you need to create a character')
+	else:
+		value = len(CharacterList)
+		return question('You have created {} characters ... you can start adventure or create another character'.format(value))
 
 @ask.intent('CharacterName')
 def characterName():
-	if(playing == False):
-		return question('What is your characters name and type. You can be a hobbit...dwarf...or wizard?')
-	else:
-		return question('You cannot create a character mid-game')
+ 	return question('What is your character name and type, you can be a hobbit...dwarf...wizard')
 
 
 @ask.intent('CharacterCreated')
@@ -67,7 +64,7 @@ def characterCreated(firstname,charatype):
 	temp.name = firstname
 	temp.type = charatype
 	CharacterList.append(temp)
-	return question('Hello {}, the great {}, you can start adventure or create another character'.format(firstname, charatype))
+	return question('Hello {}, the great {}, ... you can start adventure or create another character'.format(firstname, charatype))
 
 @ask.intent('DontStartGame')
 def no():
@@ -76,23 +73,22 @@ def no():
 @ask.intent('AdventureStart')
 def adventureStart():
 	playing = True
-	return question('Both {} and {} start your adventure in the shire it is time for your first battle'.format(CharacterList[0].name,CharacterList[1].name))
+	return question('Both {} and {} start your adventure in ... Mordor ... it is time for your first battle'.format(CharacterList[0].name,CharacterList[1].name))
 
 
 @ask.intent('BattleStart')
 def battleStart():
 	monster1.name = "Giant rat"
-	return question('You are fighting a {}, {} now choose stances'.format(monster1.name,CharacterList[0].name))
+	return question('You are fighting a {}, {} now choose your stance'.format(monster1.name,CharacterList[0].name))
 
 @ask.intent('ChooseStance')
 def chooseStance(stance):
-
 	if(CharacterList[0].stance == "NA"):
 		CharacterList[0].stance = stance
 		return question('{} you have chosen to {}, {} choose you stance'.format(CharacterList[0].name,CharacterList[0].stance,CharacterList[1].name))
 	elif(CharacterList[1].stance == "NA"):
 		CharacterList[1].stance = stance
-		return question('{} you have chosen to {}, now to start battle'.format(CharacterList[1].name,CharacterList[1].stance))
+		return question('{} you have chosen to {}, now to begin battle'.format(CharacterList[1].name,CharacterList[1].stance))
 	return question('now to start battle')
 
 
@@ -105,37 +101,44 @@ def battles():
 	if (attacked == False):
 		if (monster1.health > 0 and CharacterList[0].stance != "NA" and CharacterList[1].stance != "NA"):
 			if (CharacterList[0].stance == "attack"):
+				CharacterList[0].luck = random.randint(1, 100);
 				monster1.health = monster1.health - 20
 				result = result + CharacterList[0].name + "attacks..."
 			if (CharacterList[1].stance == "attack"):
+				CharacterList[1].luck = random.randint(1, 100);
 				monster1.health = monster1.health - 20
 				result = result + CharacterList[1].name + "attacks..."
 			attacked = True
 			CharacterList[0].stance = "NA"
 			CharacterList[1].stance = "NA"
-			return question(result + ' dealing great damage')
+
+			return question(result + ' dealing great damage, ... {} health is {}'.format(monster1.name, monster1.health))
+
 		elif (monster1.health < 0):
 			return statement('you have won battle')
 		else:
 			return question('need to pick stances')
 	elif(attacked == True and monster1.health > 0):
 		attacked = False
-		choice = test.newPrediction(1,1,1,1,1)
+
+		choice = test.newPrediction(CharacterList[0].health,CharacterList[1].health,monster1.health,CharacterList[0].luck,CharacterList[1].luck)
 		if (choice == 0):
-			CharacterList[0].health = CharacterList[0].health - 10
-			return question('{} attacts {}'.format(monster1.name, CharacterList[0].name))
+			if(CharacterList[0].stance == "attack"):
+				CharacterList[0].health = CharacterList[0].health - 10
+				return question('{} attacts {}'.format(monster1.name, CharacterList[0].name))
+			else:
+				return question('{} defends the attack'.format(CharacterList[0].name))
 		elif (choice == 1):
-			CharacterList[1].health = CharacterList[1].health - 10
-			return question('{} attacts {}'.format(monster1.name, CharacterList[1].name))
+			if(CharacterList[1].stance == "attack"):
+				CharacterList[1].health = CharacterList[1].health - 10
+				return question('{} attacts {}'.format(monster1.name, CharacterList[1].name))
+			else:
+				return question('{} defends the attack'.format(CharacterList[1].name))
 		else:
 			return question('{} regen health'.format(monster1.name))
 	else:
 
 		return question('You have won the battle well done')
-
-
-
-
 
 
 if __name__ == '__main__':
